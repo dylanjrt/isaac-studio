196e75b3e6086995d758dc1ad358a2ca7eea3937
Dylan Rochon-Terry <dylanjrt@gmail.com>
2025-06-18 22:01:41 -0400

    Add singleton contact page

---
+++ schemaTypes/contactPage.ts
@@ -0,0 +1,16 @@
+export const contactPage = {
+  name: 'contactPage',
+  title: 'Contact Page',
+  type: 'document',
+  fields: [
+    {
+      name: 'image',
+      title: 'Contact Page Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+      description: 'Image to display on the contact page',
+    },
+  ],
+}
